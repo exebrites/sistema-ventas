180fb43568bcfcbc2870f994c8702e6d221089d9
exe <exequielbrites8@gmail.com>
2025-01-02 14:35:11 -0300

    Agregar Const pedido cancelado

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -37,11 +37,18 @@ class PedidoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    const ESTADO_CANCELADO = 11;
+    // const ESTADO_ENTREGADO = 10;
     public function index()
     {
-        $estadoCancelado = 11;
-        $estadoEntregado = 10;
-        $pedidos =  Pedido::where('estado_id', '!=', $estadoCancelado)->orderBy('estado_id', 'asc')->orderBy('id', 'desc')->get();
+        // establece los estados de los pedidos
+        // $estadoCancelado = 11;
+        // $estadoEntregado = 10;
+        // obtiene los pedidos que no tengan el estado cancelado y los ordena por estado y id de creacion
+        $pedidos =  Pedido::where('estado_id', '!=', self::ESTADO_CANCELADO)->orderBy('estado_id', 'asc')->orderBy('id', 'desc')->get();
+
+        // formatea las fechas
         foreach ($pedidos as $key => $pedido) {
             $fecha =  Carbon::parse($pedido->fecha_entrega);
             $pedido->fecha_entrega = $fecha->format('d-m-Y');
@@ -51,9 +58,9 @@ class PedidoController extends Controller
                 $pedido->fecha_inicio = $fecha->format('d-m-Y');
             }
         }
-
+        //ordena los pedidos por su estado
         $pedidos = $pedidos->sortBy('estado_id');
-        // dd($pedidos);
+
         return view('pedido.index', compact('pedidos'));
     }
 
