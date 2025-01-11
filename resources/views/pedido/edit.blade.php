a42682d1d624478a75c1cb7b869ae679f313334c
exe <exequielbrites8@gmail.com>
2025-01-11 12:10:45 -0300

    Actualizar combobox view edit pedido

--- resources/views/pedido/edit.blade.php
+++ resources/views/pedido/edit.blade.php
@@ -28,59 +28,17 @@
                 <div class="form-group">
                     <label for="estado">Estado</label>
                     <select class="form-control" id="estado" name="estado" required>
-                        @if ($pedido->estado->nombre == 'en_confirmacion_imprenta')
-                            <option value="en_confirmacion_imprenta" @if ($pedido->estado->nombre == 'en_confirmacion_imprenta') selected  @endif>En
-                                confirmacion de imprenta
-                            </option>                         
-                            <option value="cancelado" @if ($pedido->estado->nombre == 'cancelado') selected @endif>Cancelado
-                            </option>
-                        @elseif($pedido->estado->nombre == 'pendiente_pago')
-                            <option value="pendiente_pago" @if ($pedido->estado->nombre == 'pendiente_pago') selected @endif>Pendiente de
-                                pago
-                            </option>
-                            <option value="confirmado_pago">Confirmado de
-                                pago
+                        @if ($pedido->estado->nombre == 'pendiente')
+                            <option value="despachado" @if ($pedido->estado->nombre == 'despachado') selected @endif>
+                                {{ $pedido->estado->descripcion }}
                             </option>
                             <option value="cancelado" @if ($pedido->estado->nombre == 'cancelado') selected @endif>Cancelado
-                            </option>
-                        @elseif ($pedido->estado->nombre == 'confirmado_pago')
-                            <option value="confirmado_pago" @if ($pedido->estado->nombre == 'confirmado_pago') selected @endif>Confirmado de
-                                pago
-                            </option>
-                            <option value="inicio">Inicio</option>
-                            <option value="cancelado">Cancelado
-                            </option>
-                        @elseif($pedido->estado->nombre == 'inicio')
-                            <option value="inicio" @if ($pedido->estado->nombre == 'inicio') selected @endif>Inicio</option>
-                            <option value="disenio">Diseño</option>
-                            <option value="cancelado">Cancelado
-                            </option>
-                        @elseif($pedido->estado->nombre == 'disenio')
-                            <option value="disenio" @if ($pedido->estado->nombre == 'disenio') selected @endif>Diseño</option>
-                            <option value="pre_produccion">Pre produccion
-                            </option>
-                            </option>
-                        @elseif($pedido->estado->nombre == 'pre_produccion')
-                            <option value="pre_produccion" @if ($pedido->estado->nombre == 'pre_produccion') selected @endif>Pre produccion
-                            </option>
-                            <option value="produccion">Produccion</option>
-                        @elseif($pedido->estado->nombre == 'produccion')
-                            <option value="produccion" @if ($pedido->estado->nombre == 'produccion') selected @endif>Produccion
-                            </option>
-                            <option value="terminado">Terminado
-                            </option>
-                        @elseif($pedido->estado->nombre == 'terminado')
-                            <option value="terminado" @if ($pedido->estado->nombre == 'terminado') selected @endif>Terminado
-                            </option>
-                            <option value="despachado">Despachado
                             </option>
                         @elseif($pedido->estado->nombre == 'despachado')
-                            <option value="despachado" @if ($pedido->estado->nombre == 'despachado') selected @endif>Despachado
+                            <option value="despachado" @if ($pedido->estado->nombre == 'despachado') selected @endif>
+                                {{ $pedido->estado->descripcion }}
                             </option>
-                            <option value="entregado">Entregado
-                            </option>
-                        @elseif($pedido->estado->nombre == 'entregado')
-                            <option value="entregado" @if ($pedido->estado->nombre == 'entregado') selected @endif>Entregado
+                            <option value="cancelado" @if ($pedido->estado->nombre == 'cancelado') selected @endif>Cancelado
                             </option>
                         @endif
                     </select>
