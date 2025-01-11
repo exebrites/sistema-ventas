c3305cdde0c7f03de63246a47e413b3ccf0f0f6f
exe <exequielbrites8@gmail.com>
2025-01-10 23:12:18 -0300

    Agregar models Cliente llamada a relacion y comentarios

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,54 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\SistOliva;
-
+use App\Models\Pedido;
 use OwenIt\Auditing\Contracts\Auditable;
-
-
 
 class Cliente extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+    use HasFactory;
+
     protected $table = "clientes";
-
-    // Indica que la clave primaria es el campo 'dni'
-    // protected $primaryKey = 'dni';
-
-    // Indica que la clave primaria no es un número entero autoincremental
-    // public $incrementing = false;
-
-    // Indica que el tipo de dato de la clave primaria es una cadena
-    // protected $keyType = 'string';
-
-    // Resto de las propiedades del modelo
     protected $fillable = ['dni', 'nombre', 'apellido', 'telefono', 'correo'];
-
-
-    /*----------------------------ATRIBUTOS----------------------------------------*/
-
-
-    /*----------------------------METODOS-----------------------------------------*/
-
-
-    /*----------------------------RELACIONES----------------------------------------*/
-
-    /*AGREGAR ATRIBUTOS*/
-
-    /*AGREGAR METODOS*/
-
-    /*AGREGAR RELACIONES*/
-
-    //SIN PROBAR 
 
     public function pedidos()
     {
-        //llamado al modelo, como es fk en tabla pedidos, como es pk
-        return $this->hasMany('\App\Models\Pedido', 'clientes_id', '');
+        return $this->hasMany(Pedido::class, 'clientes_id', '');
     }
-
-    use HasFactory;
-
-    //scopes
     public function scopeObtenerCliente($query, $user)
     {
         return $query->where('correo', $user->email)->first();
