1bac827f9b230cbfe4dcc2bd8fac29d2dda43933
exe <exequielbrites8@gmail.com>
2025-01-13 16:42:18 -0300

    Agregar ShoppingCartService

--- app/Contracts/ShoppingCartInterface.php
+++ app/Contracts/ShoppingCartInterface.php
@@ -6,9 +6,15 @@ interface ShoppingCartInterface
 {
     public function getTotal(): float;
 
-    public function getContent(): array;
+    public function getContent(): array; //cuidaodo
 
     public function getItem($id): ?object;
 
     public function clear(): void;
+
+    public function remove($id): void;
+
+    public function add(array $data): void;
+
+    public function update($id, array $data): void;
 }
---
+++ app/Services/ShoppingCartService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\ShoppingCartInterface;
+
+class ShoppingCartService implements ShoppingCartInterface
+{
+    public function getTotal(): float
+    {
+        return \Cart::getTotal();
+    }
+
+    public function getContent(): array
+    {
+        return \Cart::getContent()->toArray();
+    }
+
+    public function getItem($id): ?object
+    {
+        return \Cart::get($id);
+    }
+
+    public function clear(): void
+    {
+        \Cart::clear();
+    }
+    public function remove ($id): void
+    {
+        \Cart::remove($id);
+    }
+    public function add (array $data): void //dudoso 
+    {
+        \Cart::add($data);
+    }
+    public function update ($id, array $data): void
+    {
+        \Cart::update($id, $data);
+    }
+}
