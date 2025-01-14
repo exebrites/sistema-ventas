5c06e81619231e404345b87a952c3f38e1b3efa8
exe <exequielbrites8@gmail.com>
2025-01-14 11:30:28 -0300

    Agregar view index producto modal actualizar stock personalizacion

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -101,24 +101,41 @@
             <!-- Button trigger modal -->
 
             <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+            <form action="" method="post">
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+
+
+
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Stock Actual</label>
+                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""
+                                        value="{{ $producto->stock }}" readonly>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Cantidades</label>
+                                    <input type="number" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Ingresa cantidades" name ="stock">
+                                </div>
+
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <button type="button" class="btn btn-primary">Actualizar stock</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
+            </form>
         </div>
     </div>
 @stop
