449006db842d069262afac7f80eab211d03f48b3
exe <exequielbrites8@gmail.com>
2025-01-14 11:19:55 -0300

    Agregar view index producto modal actualizar stock

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -85,14 +85,40 @@
                                         type="submit">Borrar</button>
                                 </form>
                             </td>
-                            <td width="10px"><a class="btn btn-warning btn btn-sm btn-fixed-width"
-                                href="#">Actualizar stock</a></td>
+                            {{-- <td width="10px"><a class="btn btn-warning btn btn-sm btn-fixed-width"
+                                href="#">Actualizar stock</a></td> --}}
+                            <td> <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                    Actualizar stock
+                                </button></td>
+
                             <td width="10px"><a class="btn btn-secondary btn btn-sm btn-fixed-width"
                                     href="{{ route('productos.show', $producto->id) }}">Ver mas</a></td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+            <!-- Button trigger modal -->
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @stop
