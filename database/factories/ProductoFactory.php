d235591334d8320b2980b2f5987f8f703f031ad6
exe <exequielbrites8@gmail.com>
2025-01-21 11:16:49 -0300

    Agregar producto seeder

--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -18,14 +18,32 @@ class ProductoFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'price' => $this->faker->randomFloat(2),
-            'slug' => $this->faker->lexify(),
-            'description' => $this->faker->lexify(),
+            'nombre' => $this->faker->name(),
+            'precio' => $this->faker->randomFloat(2),
+            // 'slug' => $this->faker->lexify(),
+            'descripcion' => $this->faker->lexify(),
             'category_id' => 1,
-            'image_path' => $this->faker->imageUrl(640, 480, 'animals', true),
+            'imagen' => $this->faker->imageUrl(640, 480, 'animals', true),
             'alias' => $this->faker->lexify(),
-            'visitas' => 0
+            'visitas' => 0,
+            'sku' => $this->faker->unique()->ean13(),
+            'codigo_barras' => $this->faker->ean8(),
+            'marca' => $this->faker->company(),
+            'dimensiones' => $this->faker->randomFloat(1, 2) . ' x ' . $this->faker->randomFloat(1, 2) . ' x ' . $this->faker->randomFloat(1, 2),
+            'peso' => $this->faker->randomFloat(1, 2),
+            'material' => $this->faker->randomElement(['plástico', 'papel', 'madera']),
+            'color' => $this->faker->colorName(),
+            'formato' => $this->faker->randomElement(['Tapa dura', 'Tapa blanda', 'Digital']),
+            'tinta' => $this->faker->randomElement(['Offset', 'Digital', 'Tipograf a']),
+            'gramaje' => $this->faker->randomFloat(1, 2),
+            'tamanio' => $this->faker->randomElement(['Peque o', 'Mediano', 'Grande']),
+            'autor' => $this->faker->name(),
+            'editorial' => $this->faker->company(),
+            'numero_paginas' => $this->faker->numberBetween(1, 1000),
+            'idioma' => $this->faker->languageCode(),
+            'edicion' => $this->faker->numberBetween(1, 10),
+            'anio_publicacion' => $this->faker->year(),
+            'genero' => $this->faker->randomElement(['Acción', 'Aventura', 'Ciencia ficción', 'Fantasía', 'Misterio', 'Romance', 'Terror'])
 
         ];
     }
--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -15,68 +15,6 @@ class ProductoSeeder extends Seeder
      */
     public function run()
     {
-        //para ejecutar un seeder 
-        //php artisan db:seed --class=NombreDelSeeder
-
-
-        Producto::create([
-            'name' => 'Almanaque Anillado',
-            'alias' => 'AA14x15',
-            'slug' => 'almanaque-anillado',
-            // 'details' => '15 pulgadas, 1TB HDD, 32GB RAM',
-            'price' => 3393,
-            // 'shipping_cost' => 29.99,
-            'description' => 'Almanaque Anillado 14 x 15 cm',
-            'category_id' => 1,
-            // 'brand_id' => 1,
-            'image_path' => '/storage/dHILNFqAWu2t1RqIkeBzDasA53X5MyGmcIdCq9IV.jpg'
-        ]);
-        Producto::create([
-            'name' => 'Carpetas de Presentación',
-            'alias' => 'CP30x40',
-            'slug' => 'carpeta-presentacion',
-            // 'details' => '15 pulgadas, 1TB HDD, 32GB RAM',
-            'price' => 1950.32,
-            // 'shipping_cost' => 29.99,
-            'description' => ' 30 x 40 cm',
-            'category_id' => 1,
-            // 'brand_id' => 1,
-            'image_path' => '/storage/gRiyp82dYfxgMtYymZxmASznBk8GWWD1b6NiEOr5.jpg'
-        ]);
-
-        // \App\Models\Producto::factory(100)->create();
-        // Producto::create([
-        //     'name' => 'Volantes Flyers Folletos',
-        //     'slug' => 'flyres',
-        //     // 'details' => '15 pulgadas, 1TB HDD, 32GB RAM',
-        //     'price' => 19374.32,
-        //     // 'shipping_cost' => 29.99,
-        //     'description' => '10 x 15cm',
-        //     'category_id' => 1,
-        //     // 'brand_id' => 1,
-        //     'image_path' => 'flyers-pymedia'
-        // ]);
-        // Producto::create([
-        //     'name' => 'Señaladores',
-        //     'slug' => 'señaladores',
-        //     // 'details' => '15 pulgadas, 1TB HDD, 32GB RAM',
-        //     'price' => 19374.32,
-        //     // 'shipping_cost' => 29.99,
-        //     'description' => '5 x 15 cm',
-        //     'category_id' => 1,
-        //     // 'brand_id' => 1,
-        //     'image_path' => 'señaladores-pymedia'
-        // ]);
-        // Producto::create([
-        //     'name' => 'Tarjetas Personales',
-        //     'slug' => 'tarjetas-personales',
-        //     // 'details' => '15 pulgadas, 1TB HDD, 32GB RAM',
-        //     'price' => 218.44,
-        //     // 'shipping_cost' => 29.99,
-        //     'description' => '9 x 5 cm',
-        //     'category_id' => 1,
-        //     // 'brand_id' => 1,
-        //     'image_path' => 'tarjetas-pymedia'
-        // ]);
+        Producto::factory()->count(20)->create();
     }
 }
