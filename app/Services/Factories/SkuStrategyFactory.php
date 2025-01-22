d6e8834860a4d09779812a74375262ffe4a33365
exe <exequielbrites8@gmail.com>
2025-01-22 14:29:47 -0300

    Implementacion de Factory

--- app/Services/Factories/SkuStrategyFactory.php
+++ app/Services/Factories/SkuStrategyFactory.php
@@ -1,2 +1,22 @@
-<?php 
+<?php
+
 namespace App\Services\Factories;
+
+use App\Services\Sku\Strategies\SkuGenerationStrategy;
+use App\Services\Sku\Strategies\CategoryMaterialColorStrategy;
+use App\Services\Sku\Strategies\CategoryDimensionsAuthorStrategy;
+use invalidArgumentException;
+class SkuStrategyFactory
+{
+    public static function create(array $attributes): SkuGenerationStrategy
+    {
+        if (isset($attributes['material']) && isset($attributes['color'])) {
+            return new CategoryMaterialColorStrategy();
+        }
+        if (isset($attributes['dimensions']) && isset($attributes['author'])) {
+            return new CategoryDimensionsAuthorStrategy();
+        }
+
+        throw new InvalidArgumentException('No valid strategy found for the given attributes.');
+    }
+}
