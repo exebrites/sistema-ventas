e8b1942ab8062446abcfe93be3fb6018760e11f8
exe <exequielbrites8@gmail.com>
2025-01-22 12:01:21 -0300

    Definicion de concretstrategy CatDimAuthor

--- app/Services/Sku/Strategies/CategoryDimensionsAuthorStrategy.php
+++ app/Services/Sku/Strategies/CategoryDimensionsAuthorStrategy.php
@@ -1,2 +1,15 @@
 <?php
+
 namespace App\Services\Sku\Strategies;
+
+use App\Services\Sku\Strategies\SkuGenerationStrategy;
+
+
+class CategoryDimesionAuthorStrategy implements SkuGenerationStrategy
+{
+
+    public function generateSku(array $attributes): string
+    {
+        return "{$attributes['category']}-{$attributes['dimensions']}-{$attributes['author']}-{$attributes['id']}";
+    }
+}
