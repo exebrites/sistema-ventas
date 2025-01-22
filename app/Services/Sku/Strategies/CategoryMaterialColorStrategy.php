a2389ba05924632cf5df688ea4b47760b05ea9c4
exe <exequielbrites8@gmail.com>
2025-01-22 11:59:30 -0300

    Definicion de concretstrategy catMatColor

--- app/Services/Sku/Strategies/CategoryMaterialColorStrategy.php
+++ app/Services/Sku/Strategies/CategoryMaterialColorStrategy.php
@@ -1,2 +1,14 @@
 <?php
+
 namespace App\Services\Sku\Strategies;
+
+use App\Services\Sku\Strategies\SkuGenerationStrategy;
+
+class CategoryMaterialColorStrategy implements SkuGenerationStrategy
+{
+
+    public function generateSku(array $attributes): string{
+
+      return "{$attributes['category']}-{$attributes['material']}-{$attributes['color']}-{$attributes['year']}-{$attributes['id']}";
+    }
+}
