a2b447c115a5659c28557f713a69d6956d729642
exe <exequielbrites8@gmail.com>
2025-01-22 12:06:47 -0300

    Definicion de context strategy

--- app/Services/Sku/SkuGenerator.php
+++ app/Services/Sku/SkuGenerator.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace App\Services\Sku;
+
+use App\Services\Sku\Strategies\SkuGenerationStrategy;
+
+class SkuGenerator
+{
+    private SkuGenerationStrategy $strategy;
+    public function __construct(SkuGenerationStrategy $strategy)
+    {
+        $this->strategy  = $strategy;
+    }
+    public function generation(array $attributes): string
+    {
+        return $this->strategy->generateSku($attributes);
+    }
+}
